adaefd8826f83bec294c988d19c5813e452c3f6d
Elliotpwnz <Elliotsminion@gmail.com>
2015-07-06 14:40:11 -0400

    initial commit
    v 1.0

---
+++ 15Puzzle/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace _15Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PuzzleButton1 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton2 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton3 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton4 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton5 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton6 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton7 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton8 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton9 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton10 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton11 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton12 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton13 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton16 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton14 = new Infragistics.Win.Misc.UltraButton();
+            this.PuzzleButton15 = new Infragistics.Win.Misc.UltraButton();
+            this.SuspendLayout();
+            // 
+            // PuzzleButton1
+            // 
+            this.PuzzleButton1.AcceptsFocus = false;
+            this.PuzzleButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton1.Location = new System.Drawing.Point(12, 12);
+            this.PuzzleButton1.Name = "PuzzleButton1";
+            this.PuzzleButton1.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton1.TabIndex = 0;
+            this.PuzzleButton1.Text = "1";
+            this.PuzzleButton1.Click += new System.EventHandler(this.PuzzleButton1_Click);
+            // 
+            // PuzzleButton2
+            // 
+            this.PuzzleButton2.AcceptsFocus = false;
+            this.PuzzleButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton2.Location = new System.Drawing.Point(105, 12);
+            this.PuzzleButton2.Name = "PuzzleButton2";
+            this.PuzzleButton2.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton2.TabIndex = 1;
+            this.PuzzleButton2.Text = "2";
+            this.PuzzleButton2.Click += new System.EventHandler(this.PuzzleButton2_Click);
+            // 
+            // PuzzleButton3
+            // 
+            this.PuzzleButton3.AcceptsFocus = false;
+            this.PuzzleButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton3.Location = new System.Drawing.Point(198, 12);
+            this.PuzzleButton3.Name = "PuzzleButton3";
+            this.PuzzleButton3.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton3.TabIndex = 2;
+            this.PuzzleButton3.Text = "3";
+            this.PuzzleButton3.Click += new System.EventHandler(this.PuzzleButton3_Click);
+            // 
+            // PuzzleButton4
+            // 
+            this.PuzzleButton4.AcceptsFocus = false;
+            this.PuzzleButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton4.Location = new System.Drawing.Point(298, 12);
+            this.PuzzleButton4.Name = "PuzzleButton4";
+            this.PuzzleButton4.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton4.TabIndex = 3;
+            this.PuzzleButton4.Text = "4";
+            this.PuzzleButton4.Click += new System.EventHandler(this.PuzzleButton4_Click);
+            // 
+            // PuzzleButton5
+            // 
+            this.PuzzleButton5.AcceptsFocus = false;
+            this.PuzzleButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton5.Location = new System.Drawing.Point(12, 98);
+            this.PuzzleButton5.Name = "PuzzleButton5";
+            this.PuzzleButton5.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton5.TabIndex = 4;
+            this.PuzzleButton5.Text = "5";
+            this.PuzzleButton5.Click += new System.EventHandler(this.PuzzleButton5_Click);
+            // 
+            // PuzzleButton6
+            // 
+            this.PuzzleButton6.AcceptsFocus = false;
+            this.PuzzleButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton6.Location = new System.Drawing.Point(105, 98);
+            this.PuzzleButton6.Name = "PuzzleButton6";
+            this.PuzzleButton6.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton6.TabIndex = 5;
+            this.PuzzleButton6.Text = "6";
+            this.PuzzleButton6.Click += new System.EventHandler(this.PuzzleButton6_Click);
+            // 
+            // PuzzleButton7
+            // 
+            this.PuzzleButton7.AcceptsFocus = false;
+            this.PuzzleButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton7.Location = new System.Drawing.Point(198, 98);
+            this.PuzzleButton7.Name = "PuzzleButton7";
+            this.PuzzleButton7.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton7.TabIndex = 6;
+            this.PuzzleButton7.Text = "7";
+            this.PuzzleButton7.Click += new System.EventHandler(this.PuzzleButton7_Click);
+            // 
+            // PuzzleButton8
+            // 
+            this.PuzzleButton8.AcceptsFocus = false;
+            this.PuzzleButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton8.Location = new System.Drawing.Point(298, 98);
+            this.PuzzleButton8.Name = "PuzzleButton8";
+            this.PuzzleButton8.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton8.TabIndex = 7;
+            this.PuzzleButton8.Text = "8";
+            this.PuzzleButton8.Click += new System.EventHandler(this.PuzzleButton8_Click);
+            // 
+            // PuzzleButton9
+            // 
+            this.PuzzleButton9.AcceptsFocus = false;
+            this.PuzzleButton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton9.Location = new System.Drawing.Point(12, 184);
+            this.PuzzleButton9.Name = "PuzzleButton9";
+            this.PuzzleButton9.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton9.TabIndex = 8;
+            this.PuzzleButton9.Text = "9";
+            this.PuzzleButton9.Click += new System.EventHandler(this.PuzzleButton9_Click);
+            // 
+            // PuzzleButton10
+            // 
+            this.PuzzleButton10.AcceptsFocus = false;
+            this.PuzzleButton10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton10.Location = new System.Drawing.Point(105, 184);
+            this.PuzzleButton10.Name = "PuzzleButton10";
+            this.PuzzleButton10.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton10.TabIndex = 9;
+            this.PuzzleButton10.Text = "10";
+            this.PuzzleButton10.Click += new System.EventHandler(this.PuzzleButton10_Click);
+            // 
+            // PuzzleButton11
+            // 
+            this.PuzzleButton11.AcceptsFocus = false;
+            this.PuzzleButton11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton11.Location = new System.Drawing.Point(198, 184);
+            this.PuzzleButton11.Name = "PuzzleButton11";
+            this.PuzzleButton11.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton11.TabIndex = 10;
+            this.PuzzleButton11.Text = "11";
+            this.PuzzleButton11.Click += new System.EventHandler(this.PuzzleButton11_Click);
+            // 
+            // PuzzleButton12
+            // 
+            this.PuzzleButton12.AcceptsFocus = false;
+            this.PuzzleButton12.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton12.Location = new System.Drawing.Point(298, 184);
+            this.PuzzleButton12.Name = "PuzzleButton12";
+            this.PuzzleButton12.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton12.TabIndex = 11;
+            this.PuzzleButton12.Text = "12";
+            this.PuzzleButton12.Click += new System.EventHandler(this.PuzzleButton12_Click);
+            // 
+            // PuzzleButton13
+            // 
+            this.PuzzleButton13.AcceptsFocus = false;
+            this.PuzzleButton13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton13.Location = new System.Drawing.Point(12, 270);
+            this.PuzzleButton13.Name = "PuzzleButton13";
+            this.PuzzleButton13.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton13.TabIndex = 12;
+            this.PuzzleButton13.Text = "13";
+            this.PuzzleButton13.Click += new System.EventHandler(this.PuzzleButton13_Click);
+            // 
+            // PuzzleButton16
+            // 
+            this.PuzzleButton16.AcceptsFocus = false;
+            this.PuzzleButton16.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton16.Location = new System.Drawing.Point(298, 270);
+            this.PuzzleButton16.Name = "PuzzleButton16";
+            this.PuzzleButton16.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton16.TabIndex = 13;
+            this.PuzzleButton16.Text = " ";
+            this.PuzzleButton16.Click += new System.EventHandler(this.PuzzleButton16_Click);
+            // 
+            // PuzzleButton14
+            // 
+            this.PuzzleButton14.AcceptsFocus = false;
+            this.PuzzleButton14.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton14.Location = new System.Drawing.Point(105, 270);
+            this.PuzzleButton14.Name = "PuzzleButton14";
+            this.PuzzleButton14.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton14.TabIndex = 13;
+            this.PuzzleButton14.Text = "14";
+            this.PuzzleButton14.Click += new System.EventHandler(this.PuzzleButton14_Click);
+            // 
+            // PuzzleButton15
+            // 
+            this.PuzzleButton15.AcceptsFocus = false;
+            this.PuzzleButton15.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PuzzleButton15.Location = new System.Drawing.Point(198, 270);
+            this.PuzzleButton15.Name = "PuzzleButton15";
+            this.PuzzleButton15.Size = new System.Drawing.Size(87, 80);
+            this.PuzzleButton15.TabIndex = 14;
+            this.PuzzleButton15.Text = "15";
+            this.PuzzleButton15.Click += new System.EventHandler(this.PuzzleButton15_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 363);
+            this.Controls.Add(this.PuzzleButton15);
+            this.Controls.Add(this.PuzzleButton14);
+            this.Controls.Add(this.PuzzleButton16);
+            this.Controls.Add(this.PuzzleButton13);
+            this.Controls.Add(this.PuzzleButton12);
+            this.Controls.Add(this.PuzzleButton11);
+            this.Controls.Add(this.PuzzleButton10);
+            this.Controls.Add(this.PuzzleButton9);
+            this.Controls.Add(this.PuzzleButton8);
+            this.Controls.Add(this.PuzzleButton7);
+            this.Controls.Add(this.PuzzleButton6);
+            this.Controls.Add(this.PuzzleButton5);
+            this.Controls.Add(this.PuzzleButton4);
+            this.Controls.Add(this.PuzzleButton3);
+            this.Controls.Add(this.PuzzleButton2);
+            this.Controls.Add(this.PuzzleButton1);
+            this.Name = "Form1";
+            this.Text = "15 Puzzle";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Infragistics.Win.Misc.UltraButton PuzzleButton1;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton2;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton3;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton4;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton5;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton6;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton7;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton8;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton9;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton10;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton11;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton12;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton13;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton16;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton14;
+        private Infragistics.Win.Misc.UltraButton PuzzleButton15;
+    }
+}
+
---
+++ 15Puzzle/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _15Puzzle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PuzzleButton7_Click(object sender, EventArgs e)
+        {
+            doClick(6);
+        }
+
+        private void PuzzleButton15_Click(object sender, EventArgs e)
+        {
+            doClick(14);
+        }
+
+        private void PuzzleButton14_Click(object sender, EventArgs e)
+        {
+            doClick(13);
+        }
+
+        private void PuzzleButton13_Click(object sender, EventArgs e)
+        {
+            doClick(12);
+        }
+
+        private void PuzzleButton9_Click(object sender, EventArgs e)
+        {
+            doClick(8);
+        }
+
+        private void PuzzleButton10_Click(object sender, EventArgs e)
+        {
+            doClick(9);
+        }
+
+        private void PuzzleButton11_Click(object sender, EventArgs e)
+        {
+            doClick(10);
+        }
+
+        private void PuzzleButton12_Click(object sender, EventArgs e)
+        {
+            doClick(11);
+        }
+
+        private void PuzzleButton8_Click(object sender, EventArgs e)
+        {
+            doClick(7);
+        }
+
+        private void PuzzleButton16_Click(object sender, EventArgs e)
+        {
+            doClick(15);
+        }
+
+        private void PuzzleButton6_Click(object sender, EventArgs e)
+        {
+            doClick(5);
+        }
+
+        private void PuzzleButton5_Click(object sender, EventArgs e)
+        {
+            doClick(4);
+        }
+
+        private void PuzzleButton1_Click(object sender, EventArgs e)
+        {
+            doClick(0);
+        }
+
+        private void PuzzleButton2_Click(object sender, EventArgs e)
+        {
+            doClick(1);
+        }
+
+        private void PuzzleButton3_Click(object sender, EventArgs e)
+        {
+            doClick(2);
+        }
+
+        private void PuzzleButton4_Click(object sender, EventArgs e)
+        {
+            doClick(3);
+        }
+
+        private void doClick(int currentIndex)
+        {
+            List<Infragistics.Win.Misc.UltraButton> buttonList = new List<Infragistics.Win.Misc.UltraButton>();
+            buttonList.Add(PuzzleButton1);
+            buttonList.Add(PuzzleButton2);
+            buttonList.Add(PuzzleButton3);
+            buttonList.Add(PuzzleButton4);
+            buttonList.Add(PuzzleButton5);
+            buttonList.Add(PuzzleButton6);
+            buttonList.Add(PuzzleButton7);
+            buttonList.Add(PuzzleButton8);
+            buttonList.Add(PuzzleButton9);
+            buttonList.Add(PuzzleButton10);
+            buttonList.Add(PuzzleButton11);
+            buttonList.Add(PuzzleButton12);
+            buttonList.Add(PuzzleButton13);
+            buttonList.Add(PuzzleButton14);
+            buttonList.Add(PuzzleButton15);
+            buttonList.Add(PuzzleButton16);
+
+            if (currentIndex + 1 <= 15 && !((currentIndex + 1) % 4 == 0))
+            {
+                if (buttonList[currentIndex + 1].Text == " ")
+                {
+                    buttonList[currentIndex + 1].Text = buttonList[currentIndex].Text;
+                    buttonList[currentIndex].Text = " ";
+                }
+            }
+            if (currentIndex - 1 >= 0 && !((currentIndex) % 4 == 0))
+            {
+                if (buttonList[currentIndex - 1].Text == " ")
+                {
+                    buttonList[currentIndex - 1].Text = buttonList[currentIndex].Text;
+                    buttonList[currentIndex].Text = " ";
+                }
+            }
+            if (currentIndex - 4 >= 0)
+            {
+                if (buttonList[currentIndex - 4].Text == " ")
+                {
+                    buttonList[currentIndex - 4].Text = buttonList[currentIndex].Text;
+                    buttonList[currentIndex].Text = " ";
+                }
+            }
+            if (currentIndex + 4 <= 15)
+            {
+                if (buttonList[currentIndex + 4].Text == " ")
+                {
+                    buttonList[currentIndex + 4].Text = buttonList[currentIndex].Text;
+                    buttonList[currentIndex].Text = " ";
+                }
+            }
+
+           
+            
+        }
+
+    }
+}
